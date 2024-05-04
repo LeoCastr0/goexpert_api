27227cd768eb835a3ceaa60d2cfd18988d6d3ba0
LeoCastr0 <leonardofagundescastro@gmail.com>
2024-05-03 23:00:14 -0300

    API using GO from the go expert MBA

---
+++ cmd/server/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/jwtauth"
+	"github.com/leocastr0/goexpert/api/configs"
+	_ "github.com/leocastr0/goexpert/api/docs"
+	"github.com/leocastr0/goexpert/api/internal/entity"
+	"github.com/leocastr0/goexpert/api/internal/infra/database"
+	"github.com/leocastr0/goexpert/api/internal/webserver/handlers"
+	httpSwagger "github.com/swaggo/http-swagger"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+// @title           Pos Go Expert API
+// @version         1.0
+// @description     This is the API developed in the course following the learned content.
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name   Leonardo F. De Castro
+// @contact.url    http://www.swagger.io/support
+// @contact.email  leonardofagundescastro@gmail.com
+
+// @license.name  Leonardo F. de Castro License
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host      localhost:8000
+// @BasePath  /
+
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
+
+func main() {
+	configs, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+
+	productDB := database.NewProduct(db)
+	productHandler := handlers.NewProductHandler(productDB)
+
+	userDB := database.NewUser(db)
+	userHandler := handlers.NewUserHandler(userDB)
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger) // intercepta todas as requisicoes e injeta logs
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.WithValue("jwt", configs.TokenAuth))
+	r.Use(middleware.WithValue("JwtExpiresIn", configs.JWTExpiresIn))
+	//r.Use(LogRequest)
+
+	r.Route("/products", func(r chi.Router) {
+		r.Use(jwtauth.Verifier(configs.TokenAuth))
+		r.Use(jwtauth.Authenticator)
+		r.Post("/", productHandler.CreateProduct) // usando router
+		r.Get("/{id}", productHandler.GetProduct)
+		r.Get("/", productHandler.GetProducts)
+		r.Put("/{id}", productHandler.UpdateProduct)
+		r.Delete("/{id}", productHandler.DeleteProduct)
+	})
+
+	r.Post("/users", userHandler.CreateUser)
+	r.Post("/users/generate_token", userHandler.GetJWT)
+
+	r.Get("/docs/*", httpSwagger.Handler(httpSwagger.URL("localhost:8000/docs/doc.json")))
+
+	//http.HandleFunc("/products", productHandler.CreateProduct) // sem utilizar router
+	http.ListenAndServe(":8000", r)
+}
+
+func LogRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Request: %s %s", r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
+}
